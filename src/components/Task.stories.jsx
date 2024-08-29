c981e0a7bb9bed9c7fc4202949b619f4750b6cb3
Christian <chrisdb150@gmail.com>
2024-08-29 19:53:49 -0400

    resolving bug

--- src/components/Task.stories.jsx
+++ src/components/Task.stories.jsx
@@ -7,6 +7,7 @@ export const ActionsData = {
   onPinTask: fn(),
 };
 
+
 export default {
   component: Task,
   title: 'Task',
@@ -42,6 +43,18 @@ export const Archived = {
     task: {
       ...Default.args.task,
       state: 'TASK_ARCHIVED',
+    },
+  },
+};
+
+
+const longTitleString = `This task's name is absurdly large. In fact, I think if I keep going I might end up with content overflow. What will happen? The star that represents a pinned task could have text overlapping. The text could cut-off abruptly when it reaches the star. I hope not!`;
+
+export const LongTitle = {
+  args: {
+    task: {
+      ...Default.args.task,
+      title: longTitleString,
     },
   },
 };
